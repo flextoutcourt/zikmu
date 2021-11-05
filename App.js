e7fc2b264ebfc332958f6c2ac4cca67315d15433
Flextoutcourt <flextoutcourt@gmail.com>
2021-11-05 16:45:46 +0100

    adding library tracks data (test branchement api)

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {FlatList, SafeAreaView, StatusBar, StyleSheet, Text, useColorScheme, View} from 'react-native';
+import {FlatList, SafeAreaView, StatusBar, StyleSheet, Text, useColorScheme, View, Image, ScrollView, ListView} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
@@ -7,8 +7,8 @@ const App = () => {
 
   const [tracks, setTracks] = useState();
 
-  const _get_tracks = () => {
-    fetch("https://api.spotify.com/v1/me/tracks?market=FR&offset=0", {
+  const _get_tracks = (offset = 0) => {
+    fetch(`https://api.spotify.com/v1/me/tracks?market=FR&limit=10&offset=${offset}`, {
 		headers: {
 			Accept: "application/json",
 			Authorization: "Bearer BQDdnSY8XrrbYY_DFV-jemgPmyBx4zLOsgNuPFRs7NLXyjT85j8NwSXa1LpkTCSffVTMiO_0dHRvUKrDxCDKgCa75lAuRPyGCSJA8vLbfyG8phq0XF05XBB2J1nmk2LcjRR2KPfZeBXZYuVUmBiwJM3TgY_-eSWXk4F_a5UpDDHXCl4QBbv0yqIgKrSWIYeoUn9VA96ZzU8DEmN16euNuVnDrZgaC6Q3RSjpseu0RGbgzvgnAhvumsT_HtA4FzI826VC8DpcMO1aMxCIS5TcCqZI9VzSnFSvKP17Ki8dgyv3",
@@ -32,7 +32,7 @@ const App = () => {
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <View
+        <ScrollView
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
@@ -41,11 +41,22 @@ const App = () => {
           </Text>
           {
               <FlatList
+			  	style={{display: "flex", flexDirection:'row'}}
                 data={tracks?.items}
-                renderItem={({item, key}) => <Text style={styles.item}>{item.track.name}</Text>}
+			  	scrollEnabled={true}
+				horizontal={true}
+				onEndReachedThreshold={10}
+				// onEndReached={_get_tracks(tracks?.items?.length)}
+                renderItem={({item, key}) => (
+					<View style={{width: 150, padding: 5, backgroundColor: 'red', margin: 5}}>
+						<Text style={{maxWidth: 150}}>{item.track.name}</Text>
+						<Image source={{uri: item.track.album.images[0].url}}
+						style={{width: "auto", height: 150, margin: "auto"}} />
+					</View>
+				)}
               />
           }
-        </View>
+        </ScrollView>
     </SafeAreaView>
   );
 };
