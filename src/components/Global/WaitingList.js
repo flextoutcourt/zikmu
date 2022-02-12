07b122958a61a81e095c519452f292374702456a
Quentin Leclerc <quentin@ewill.fr>
2022-02-12 23:33:05 +0100

    refactor player structure

--- src/components/Global/WaitingList.js
+++ src/components/Global/WaitingList.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import {Dimensions, Text} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import React, {PureComponent} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 import {connect} from 'react-redux';
 
-class WaitingList extends React.PureComponent {
-	render() {
-		return (
-			<SafeAreaView
-				style={{
-					flex: 1,
-					justifyContent: 'space-between',
-					alignItems: 'flex-start',
-					width: Dimensions.get('screen').width,
-				}}>
-				<Text/>
-			</SafeAreaView>
-		);
+class WaitingList extends PureComponent{
+
+	constructor(props){
+		super(props);
+		this.state = {
+			test: null
+		}
 	}
+
+	render(){
+		return(
+			<View>
+				<Text>Waiting List menu</Text>
+			</View>
+		)
+	}
+
 }
 
 const mapStateToProps = store => {
 	return {
-		store: store,
-	};
-};
+		store: store
+	}
+}
 
 export default connect(mapStateToProps)(WaitingList);
