dcf618889e2413b45cc558b1c5ced50a96b7967d
Flextoutcourt <flextoutcourt@gmail.com>
2022-03-06 20:58:14 +0100

    #FLEX-64 add onesignal push-notifications

--- index.js
+++ index.js
@@ -6,4 +6,29 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import OneSignal from 'react-native-onesignal';
+
+OneSignal.setLogLevel(6, 0);
+OneSignal.setAppId("046417d8-d5df-4ade-b19d-91e2954b6790");
+
+//Prompt for push on iOS
+OneSignal.promptForPushNotificationsWithUserResponse(response => {
+
+});
+
+//Method for handling notifications received while app in foreground
+OneSignal.setNotificationWillShowInForegroundHandler(notificationReceivedEvent => {
+    console.log("OneSignal: notification will show in foreground:", notificationReceivedEvent);
+    let notification = notificationReceivedEvent.getNotification();
+    const data = notification.additionalData
+
+    // Complete with null means don't show a notification.
+    notificationReceivedEvent.complete(notification);
+});
+
+//Method for handling notifications opened
+OneSignal.setNotificationOpenedHandler(notification => {
+
+});
+
 AppRegistry.registerComponent(appName, () => App);
